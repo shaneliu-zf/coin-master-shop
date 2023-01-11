976aa0336a10d1e283defcf273f463df5a938c43
tyk <110590015@ntut.org.tw>
2023-01-11 13:31:32 +0800

    search

--- src/normal-page/Search.js
+++ src/normal-page/Search.js
@@ -1,27 +1,42 @@
-import {Link, useParams} from "react-router-dom";
-import {Col, Container, Row} from "react-bootstrap";
+import { Link, useParams } from "react-router-dom";
+import { Col, Container, Row } from "react-bootstrap";
 import React from "react";
 
-export default function Search_page(){
+import '../css/style.css'
+import '../css/search.css'
+
+import search_icon from '../image/search-icon.webp'
+
+export default function Search_page() {
     let { search_post } = useParams();
+
     return (
-        <div>
+        <div className="search_bg">
             <Row>
+                <Col sm={4}>
+                    <div>
+                        <input id='search_input' className="form-control" type="text" placeholder={search_post} aria-label="Search" />
+                    </div>
+                </Col>
+                <div style={{ borderRadius: "10px", padding: "3px", width: "32px" }}>
+                    <img src={search_icon} width={"25px"} />
+                </div>
+
                 <Col sm={12}>
-                    <Container>
+                    <br></br>
+                    maybe you want to search : <a href="https://coin-shoppingweb.web.app/coin/0">1921 Argent Morgan Dollar Classé Par PCGS Comme MS64</a>
 
-                        <br /><br /><br />
-                        <br /><br /><br />
-                        <br /><br /><br />
-                        <br /><br /><br />
-                        <br /><br /><br />
-                        <br /><br /><br />
-                        <br /><br /><br />
-                        <br /><br /><br />
-                        your search: {search_post}
-                        <br /><br /><br />
-                        <br /><br /><br /><br /><br /><br />
+                </Col>
 
+                <Col sm={12}>
+                    <hr></hr>
+                    <br></br><br></br><br></br>
+                </Col>
+                <Col sm={12} >
+                    <Container className='center'>
+                        <h3>
+                            Search not found
+                        </h3>
 
                     </Container>
                 </Col>
